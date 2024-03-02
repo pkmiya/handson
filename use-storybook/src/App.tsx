a7e0348a281814a09e28c2fac76481e4fc9e13c1
pkmiya <xbzd.cnlv@icloud.com>
2024-03-03 02:59:30 +0900

    use tailwindcss

--- use-storybook/src/App.tsx
+++ use-storybook/src/App.tsx
@@ -1,34 +1,43 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState } from 'react'
+import logo from '~/assets/react.svg'
 
-function App() {
+const App: React.VFC = () => {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+    <div className='text-center'>
+      <header className='bg-slate-700 min-h-screen flex flex-col align-center justify-center text-3xl text-white'>
+        <img src={logo} className='h-72' alt='logo' />
+        <p className='text-4xl'>Hello Vite + React!</p>
+        <p className='my-5'>
+          count is:
+          <button
+            type='button'
+            className='bg-gray-50 hover:bg-gray-100 text-black p-2 mx-2'
+            onClick={() => setCount((count) => count + 1)}
+          >
+            {count}
+          </button>
         </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+        <p className='my-2'>
+          Edit <code>App.tsx</code> and save to test HMR updates.
+        </p>
+        <p className='my-2'>
+          <a className='text-cyan-300' href='https://reactjs.org' target='_blank' rel='noopener noreferrer'>
+            Learn React
+          </a>
+          {' | '}
+          <a
+            className='text-cyan-300'
+            href='https://vitejs.dev/guide/features.html'
+            target='_blank'
+            rel='noopener noreferrer'
+          >
+            Vite Docs
+          </a>
+        </p>
+      </header>
+    </div>
   )
 }
 
