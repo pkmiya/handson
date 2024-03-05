525016b04a23ee54d6715748e01a2c56fb5991c4
pkmiya <xbzd.cnlv@icloud.com>
2024-03-06 01:47:08 +0900

    fix typo

--- lt-docker/1_docker-run/run.sh
+++ lt-docker/1_docker-run/run.sh
@@ -1,4 +1,4 @@
 # sh ./run.sh
-docker network create wordpresss000net1
+docker network create wordpress000net1
 docker run --name mysql000ex11 -dit --net=wordpress000net1 -e MYSQL_ROOT_PASSWORD=myrootpass -e MYSQL_DATABASE=wordpress000db -e MYSQL_USER=wordpress000kun -e MYSQL_PASSWORD=wkunpass mysql --character-set-server=utf8mb4 --collation-server=utf8mb4_unicode_ci --default-authentication-plugin=mysql_native_password
 docker run --name wordpress000ex12 -dit --net=wordpress000net1 -p 8085:80 -e WORDPRESS_DB_HOST=mysql000ex11 -e WORDPRESS_DB_NAME=wordpress000db -e WORDPRESS_DB_USER=wordpress000kun -e WORDPRESS_DB_PASSWORD=wkunpass wordpress
