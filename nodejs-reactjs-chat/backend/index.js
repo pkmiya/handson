39c8ce045fd5ee5cdbfd08e70bbb44c0b3d60dde
pkmiya <xbzd.cnlv@icloud.com>
2024-07-12 00:52:13 +0900

    run prettier

--- nodejs-reactjs-chat/backend/index.js
+++ nodejs-reactjs-chat/backend/index.js
@@ -1,6 +1,6 @@
-const express = require("express");
-const cors = require("cors");
-const axios = require("axios");
+const express = require('express');
+const cors = require('cors');
+const axios = require('axios');
 require('dotenv').config();
 
 const app = express();
@@ -8,40 +8,39 @@ app.use(express.json());
 app.use(cors({ origin: true }));
 
 const PORT_NUMBER = 3001;
-const API_URL = "https://api.chatengine.io/users/"
+const API_URL = 'https://api.chatengine.io/users/';
 const CHAT_ENGINE_PRIVATE_KEY = process.env.CHAT_ENGINE_PRIVATE_KEY;
 
-app.post("/authenticate", async (req, res) => {
-    const { username } = req.body;
-    if (!username) {
-        return res.status(400).json({ error: "Username is required" });
-    }
+app.post('/authenticate', async (req, res) => {
+  const { username } = req.body;
+  if (!username) {
+    return res.status(400).json({ error: 'Username is required' });
+  }
 
-
-    try {
-        const response = await axios.put(API_URL,
-            {
-                "username": username,
-                "secret": username,
-            },
-            {
-                headers: {
-                    "private-key": CHAT_ENGINE_PRIVATE_KEY,
-                },
-            }
-        )
-        return res.status(response.status).json(response.data);
-    }
-    catch (error) {
-        console.error("Error details:", error);
-        if (error.response) {
-            return res.status(error.response.status).json(error.response.data);
-        } else {
-            return res.status(500).json({ error: "Internal Server Error" });
+  try {
+    const response = await axios.put(
+      API_URL,
+      {
+        username: username,
+        secret: username
+      },
+      {
+        headers: {
+          'Private-Key': CHAT_ENGINE_PRIVATE_KEY
         }
+      }
+    );
+    return res.status(response.status).json(response.data);
+  } catch (error) {
+    console.error('Error details:', error);
+    if (error.response) {
+      return res.status(error.response.status).json(error.response.data);
+    } else {
+      return res.status(500).json({ error: 'Internal Server Error' });
     }
+  }
 });
 
 app.listen(PORT_NUMBER, () => {
-    console.log(`Server is running on port ${PORT_NUMBER}`);
+  console.log(`Server is running on port ${PORT_NUMBER}`);
 });
