270d553a5674aa8fe5af05c47db9f69fee8d2aac
pkmiya <xbzd.cnlv@icloud.com>
2024-07-11 23:49:59 +0900

    backend: implement auth (create/get user)

---
+++ nodejs-reactjs-chat/backend/index.js
@@ -0,0 +1,47 @@
+const express = require("express");
+const cors = require("cors");
+const axios = require("axios");
+require('dotenv').config();
+
+const app = express();
+app.use(express.json());
+app.use(cors({ origin: true }));
+
+const PORT_NUMBER = 3001;
+const API_URL = "https://api.chatengine.io/users/"
+const CHAT_ENGINE_PRIVATE_KEY = process.env.CHAT_ENGINE_PRIVATE_KEY;
+
+app.post("/authenticate", async (req, res) => {
+    const { username } = req.body;
+    if (!username) {
+        return res.status(400).json({ error: "Username is required" });
+    }
+
+
+    try {
+        const response = await axios.put(API_URL,
+            {
+                "username": username,
+                "secret": username,
+            },
+            {
+                headers: {
+                    "private-key": CHAT_ENGINE_PRIVATE_KEY,
+                },
+            }
+        )
+        return res.status(response.status).json(response.data);
+    }
+    catch (error) {
+        console.error("Error details:", error);
+        if (error.response) {
+            return res.status(error.response.status).json(error.response.data);
+        } else {
+            return res.status(500).json({ error: "Internal Server Error" });
+        }
+    }
+});
+
+app.listen(PORT_NUMBER, () => {
+    console.log(`Server is running on port ${PORT_NUMBER}`);
+});
