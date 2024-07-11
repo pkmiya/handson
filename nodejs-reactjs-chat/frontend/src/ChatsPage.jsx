a83eada0dab28ce2e41ccddfa3b927ed11800660
pkmiya <xbzd.cnlv@icloud.com>
2024-07-12 01:14:44 +0900

    fix: delete proejct id

--- nodejs-reactjs-chat/frontend/src/ChatsPage.jsx
+++ nodejs-reactjs-chat/frontend/src/ChatsPage.jsx
@@ -2,9 +2,7 @@ import PropTypes from 'prop-types';
 
 import { PrettyChatWindow } from 'react-chat-engine-pretty';
 
-// const CHAT_ENGINE_PRIVATE_KEY = import.meta.env.CHAT_ENGINE_PRIVATE_KEY;
 const PROJECT_ID = import.meta.env.VITE_CHAT_PROJECT_ID;
-// const PROJECT_ID = '6e6cd57d-0459-4654-b023-429f481829c8';
 
 const ChatsPage = props => {
   return (
